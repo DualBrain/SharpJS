ce2ea4c6a97366f0bb036170cff4babc62af2701
0xFireball <0xFireball@outlook.com>
2016-10-02 09:02:32 -0700

    preliminary EditText implementation

---
+++ ExaPhaser/Frameworks/IridiumIon.NeptuniumKit/Controls/EditText.cs
@@ -0,0 +1,23 @@
+﻿namespace IridiumIon.NeptuniumKit.Controls
+{
+    /// <summary>
+    /// TODO!
+    /// </summary>
+    public class EditText : TextView
+    {
+        /// <summary>
+        /// A placeholder to be shown when the text is empty.
+        /// </summary>
+        public string Placeholder
+        {
+            get
+            {
+                return UnderlyingJQElement.Attr("placeholder");
+            }
+            set
+            {
+                UnderlyingJQElement.Attr("placeholder", value);
+            }
+        }
+    }
+}
